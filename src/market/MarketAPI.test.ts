1630c3b1a1ff2b571510262702db8a5f66eaa081
Benny Neugebauer <bn@bennyn.de>
2020-12-08 00:46:24 +0100

    feat: Get market details (#7)

--- src/market/MarketAPI.test.ts
+++ src/market/MarketAPI.test.ts
@@ -81,7 +81,6 @@ describe('MarketAPI', () => {
     it('returns all top-level nodes (market categories) in the market navigation hierarchy', async () => {
       nock(APIClient.URL_DEMO)
         .get(MarketAPI.URL.MARKETNAVIGATION)
-        .query(true)
         .reply(
           200,
           JSON.stringify({
@@ -138,6 +137,392 @@ describe('MarketAPI', () => {
       const marketCategories = await global.client.rest.market.getMarketCategories(nodeId);
       expect(marketCategories.nodes).toBeNull();
       expect(marketCategories.markets![0].marketStatus).toBe('TRADEABLE');
+    });
+  });
+
+  describe('getMarketDetails', () => {
+    it('returns the details of the given market', async () => {
+      const epic = 'IX.D.FTSE.CFD.IP';
+
+      nock(APIClient.URL_DEMO)
+        .get(`${MarketAPI.URL.MARKETS}/${epic}`)
+        .reply(
+          200,
+          JSON.stringify({
+            dealingRules: {
+              marketOrderPreference: 'AVAILABLE_DEFAULT_OFF',
+              maxStopOrLimitDistance: {
+                unit: 'PERCENTAGE',
+                value: 75,
+              },
+              minControlledRiskStopDistance: {
+                unit: 'POINTS',
+                value: 8,
+              },
+              minDealSize: {
+                unit: 'POINTS',
+                value: 0.25,
+              },
+              minNormalStopOrLimitDistance: {
+                unit: 'POINTS',
+                value: 8,
+              },
+              minStepDistance: {
+                unit: 'POINTS',
+                value: 1,
+              },
+              trailingStopsPreference: 'AVAILABLE',
+            },
+            instrument: {
+              chartCode: 'UKX',
+              contractSize: '10',
+              controlledRiskAllowed: true,
+              country: 'GB',
+              currencies: [
+                {
+                  baseExchangeRate: 0.9064129,
+                  code: 'GBP',
+                  exchangeRate: 1,
+                  isDefault: false,
+                  symbol: '£',
+                },
+              ],
+              epic: 'IX.D.FTSE.CFD.IP',
+              expiry: '-',
+              expiryDetails: null,
+              forceOpenAllowed: true,
+              limitedRiskPremium: {
+                unit: 'POINTS',
+                value: 1,
+              },
+              lotSize: 10,
+              marginDepositBands: [
+                {
+                  currency: 'GBP',
+                  margin: 5,
+                  max: 15,
+                  min: 0,
+                },
+                {
+                  currency: 'GBP',
+                  margin: 5,
+                  max: 150,
+                  min: 15,
+                },
+                {
+                  currency: 'GBP',
+                  margin: 5,
+                  max: 225,
+                  min: 150,
+                },
+                {
+                  currency: 'GBP',
+                  margin: 15,
+                  max: null,
+                  min: 225,
+                },
+              ],
+              marginFactor: 5,
+              marginFactorUnit: 'PERCENTAGE',
+              marketId: 'FT100',
+              name: 'FTSE 100 Kassa (10£)',
+              newsCode: '.FTSE',
+              onePipMeans: '1 Index Point',
+              openingHours: null,
+              rolloverDetails: null,
+              slippageFactor: {
+                unit: 'pct',
+                value: 50,
+              },
+              specialInfo: [
+                'MIN KNOCK OUT LEVEL DISTANCE',
+                'MAX KNOCK OUT LEVEL DISTANCE',
+                'DEFAULT KNOCK OUT LEVEL DISTANCE',
+                '24-Std. notiert- zugrunde liegender Markt handelt von 08:00 - 16:35 Mittlere Greenwich Zeit (MGZ)',
+              ],
+              sprintMarketsMaximumExpiryTime: null,
+              sprintMarketsMinimumExpiryTime: null,
+              stopsLimitsAllowed: true,
+              streamingPricesAvailable: true,
+              type: 'INDICES',
+              unit: 'CONTRACTS',
+              valueOfOnePip: '10.00',
+            },
+            snapshot: {
+              bid: 6524.8,
+              binaryOdds: null,
+              controlledRiskExtraSpread: 1,
+              decimalPlacesFactor: 1,
+              delayTime: 0,
+              high: 6567.3,
+              low: 6517,
+              marketStatus: 'TRADEABLE',
+              netChange: -33.3,
+              offer: 6528.8,
+              percentageChange: -0.51,
+              scalingFactor: 1,
+              updateTime: '23:05:06',
+            },
+          })
+        );
+
+      const marketDetails = await global.client.rest.market.getMarketDetails(epic);
+      expect(marketDetails.snapshot.bid).toBe(6524.8);
+    });
+
+    it('returns the details of multiple epics', async () => {
+      const epics = ['IX.D.FTSE.CFD.IP', 'SE.D.PFE.CASH.IP'];
+
+      nock(APIClient.URL_DEMO)
+        .get(`${MarketAPI.URL.MARKETS}?epics=${encodeURIComponent(epics.join(','))}`)
+        .reply(
+          200,
+          JSON.stringify({
+            marketDetails: [
+              {
+                dealingRules: {
+                  marketOrderPreference: 'AVAILABLE_DEFAULT_OFF',
+                  maxStopOrLimitDistance: {
+                    unit: 'PERCENTAGE',
+                    value: 75,
+                  },
+                  minControlledRiskStopDistance: {
+                    unit: 'PERCENTAGE',
+                    value: 2,
+                  },
+                  minDealSize: {
+                    unit: 'POINTS',
+                    value: 0.25,
+                  },
+                  minNormalStopOrLimitDistance: {
+                    unit: 'POINTS',
+                    value: 8,
+                  },
+                  minStepDistance: {
+                    unit: 'POINTS',
+                    value: 1,
+                  },
+                  trailingStopsPreference: 'AVAILABLE',
+                },
+                instrument: {
+                  chartCode: 'UKX',
+                  contractSize: '10',
+                  controlledRiskAllowed: true,
+                  country: 'GB',
+                  currencies: [
+                    {
+                      baseExchangeRate: 0.90695953,
+                      code: 'GBP',
+                      exchangeRate: 1,
+                      isDefault: false,
+                      symbol: '£',
+                    },
+                  ],
+                  epic: 'IX.D.FTSE.CFD.IP',
+                  expiry: '-',
+                  expiryDetails: null,
+                  forceOpenAllowed: true,
+                  limitedRiskPremium: {
+                    unit: 'POINTS',
+                    value: 1,
+                  },
+                  lotSize: 10,
+                  marginDepositBands: [
+                    {
+                      currency: 'GBP',
+                      margin: 5,
+                      max: 15,
+                      min: 0,
+                    },
+                    {
+                      currency: 'GBP',
+                      margin: 5,
+                      max: 150,
+                      min: 15,
+                    },
+                    {
+                      currency: 'GBP',
+                      margin: 5,
+                      max: 225,
+                      min: 150,
+                    },
+                    {
+                      currency: 'GBP',
+                      margin: 15,
+                      max: null,
+                      min: 225,
+                    },
+                  ],
+                  marginFactor: 5,
+                  marginFactorUnit: 'PERCENTAGE',
+                  marketId: 'FT100',
+                  name: 'FTSE 100 Kassa (10£)',
+                  newsCode: '.FTSE',
+                  onePipMeans: '1 Index Point',
+                  openingHours: null,
+                  rolloverDetails: null,
+                  slippageFactor: {
+                    unit: 'pct',
+                    value: 50,
+                  },
+                  specialInfo: [
+                    'MIN KNOCK OUT LEVEL DISTANCE',
+                    'MAX KNOCK OUT LEVEL DISTANCE',
+                    'DEFAULT KNOCK OUT LEVEL DISTANCE',
+                    '24-Std. notiert- zugrunde liegender Markt handelt von 08:00 - 16:35 Mittlere Greenwich Zeit (MGZ)',
+                  ],
+                  sprintMarketsMaximumExpiryTime: null,
+                  sprintMarketsMinimumExpiryTime: null,
+                  stopsLimitsAllowed: true,
+                  streamingPricesAvailable: true,
+                  type: 'INDICES',
+                  unit: 'CONTRACTS',
+                  valueOfOnePip: '10.00',
+                },
+                snapshot: {
+                  bid: 6523.2,
+                  binaryOdds: null,
+                  controlledRiskExtraSpread: 1,
+                  decimalPlacesFactor: 1,
+                  delayTime: 0,
+                  high: 6567.3,
+                  low: 6517,
+                  marketStatus: 'TRADEABLE',
+                  netChange: -34.9,
+                  offer: 6527.2,
+                  percentageChange: -0.53,
+                  scalingFactor: 1,
+                  updateTime: '23:25:45',
+                },
+              },
+              {
+                dealingRules: {
+                  marketOrderPreference: 'AVAILABLE_DEFAULT_OFF',
+                  maxStopOrLimitDistance: {
+                    unit: 'PERCENTAGE',
+                    value: 90,
+                  },
+                  minControlledRiskStopDistance: {
+                    unit: 'PERCENTAGE',
+                    value: 20,
+                  },
+                  minDealSize: {
+                    unit: 'POINTS',
+                    value: 1,
+                  },
+                  minNormalStopOrLimitDistance: {
+                    unit: 'POINTS',
+                    value: 10,
+                  },
+                  minStepDistance: {
+                    unit: 'POINTS',
+                    value: 1,
+                  },
+                  trailingStopsPreference: 'AVAILABLE',
+                },
+                instrument: {
+                  chartCode: 'PFE',
+                  contractSize: null,
+                  controlledRiskAllowed: true,
+                  country: 'US',
+                  currencies: [
+                    {
+                      baseExchangeRate: 1.211207,
+                      code: 'USD',
+                      exchangeRate: 0.77,
+                      isDefault: false,
+                      symbol: '$',
+                    },
+                  ],
+                  epic: 'SE.D.PFE.CASH.IP',
+                  expiry: '-',
+                  expiryDetails: null,
+                  forceOpenAllowed: true,
+                  limitedRiskPremium: {
+                    unit: 'PERCENTAGE',
+                    value: 1,
+                  },
+                  lotSize: 0.01,
+                  marginDepositBands: [
+                    {
+                      currency: 'USD',
+                      margin: 20,
+                      max: 51200,
+                      min: 0,
+                    },
+                    {
+                      currency: 'USD',
+                      margin: 20,
+                      max: 256000,
+                      min: 51200,
+                    },
+                    {
+                      currency: 'USD',
+                      margin: 40,
+                      max: 1024000,
+                      min: 256000,
+                    },
+                    {
+                      currency: 'USD',
+                      margin: 75,
+                      max: null,
+                      min: 1024000,
+                    },
+                  ],
+                  marginFactor: 20,
+                  marginFactorUnit: 'PERCENTAGE',
+                  marketId: 'PFE-US',
+                  name: 'Pfizer Inc (All Sessions)',
+                  newsCode: 'PFE.N',
+                  onePipMeans: null,
+                  openingHours: {
+                    marketTimes: [
+                      {
+                        closeTime: '01:00',
+                        openTime: '09:00',
+                      },
+                    ],
+                  },
+                  rolloverDetails: null,
+                  slippageFactor: {
+                    unit: 'pct',
+                    value: 100,
+                  },
+                  specialInfo: [
+                    'MAX KNOCK OUT LEVEL DISTANCE',
+                    'DEFAULT KNOCK OUT LEVEL DISTANCE',
+                    'Please note US (All Sessions) shares close early at 22:00 UK time on Friday evenings.',
+                  ],
+                  sprintMarketsMaximumExpiryTime: null,
+                  sprintMarketsMinimumExpiryTime: null,
+                  stopsLimitsAllowed: true,
+                  streamingPricesAvailable: false,
+                  type: 'SHARES',
+                  unit: 'SHARES',
+                  valueOfOnePip: null,
+                },
+                snapshot: {
+                  bid: null,
+                  binaryOdds: null,
+                  controlledRiskExtraSpread: 0,
+                  decimalPlacesFactor: 4,
+                  delayTime: 0,
+                  high: 41.4,
+                  low: 40.34,
+                  marketStatus: 'TRADEABLE',
+                  netChange: 1.01,
+                  offer: null,
+                  percentageChange: 2.5,
+                  scalingFactor: 100,
+                  updateTime: '23:25:13',
+                },
+              },
+            ],
+          })
+        );
+
+      const {marketDetails} = await global.client.rest.market.getMarketDetails(epics);
+      expect(marketDetails[1].snapshot.high).toBe(41.4);
     });
   });
 });
--- src/market/MarketAPI.ts
+++ src/market/MarketAPI.ts
@@ -1,5 +1,152 @@
 import {AxiosInstance} from 'axios';
 
+export interface Currency {
+  baseExchangeRate: number;
+  code: string;
+  exchangeRate: number;
+  isDefault: boolean;
+  symbol: string;
+}
+
+export interface MarginDepositBand {
+  currency: string;
+  margin: number;
+  max?: number;
+  min: number;
+}
+
+export interface SlippageFactor {
+  unit: string;
+  value: number;
+}
+
+export interface LimitedRiskPremium {
+  unit: string;
+  value: number;
+}
+
+export interface ExpiryDetail {
+  lastDealingDate: string;
+  settlementInfo: string;
+}
+
+export interface RolloverDetail {
+  lastRolloverTime: string;
+  rolloverInfo: string;
+}
+
+export interface Instrument {
+  chartCode: string;
+  contractSize: string;
+  controlledRiskAllowed: boolean;
+  country: string;
+  currencies: Currency[];
+  epic: string;
+  expiry: string;
+  expiryDetails?: ExpiryDetail;
+  forceOpenAllowed: boolean;
+  limitedRiskPremium: LimitedRiskPremium;
+  lotSize: number;
+  marginDepositBands: MarginDepositBand[];
+  marginFactor: number;
+  marginFactorUnit: string;
+  marketId: string;
+  name: string;
+  newsCode: string;
+  onePipMeans: string;
+  openingHours?: {
+    marketTimes: string[];
+  };
+  rolloverDetails?: RolloverDetail;
+  slippageFactor: SlippageFactor;
+  specialInfo: string[];
+  /** For sprint markets only, the maximum value to be specified as the expiry of a sprint markets trade. */
+  sprintMarketsMaximumExpiryTime?: number;
+  /** For sprint markets only, the minimum value to be specified as the expiry of a sprint markets trade. */
+  sprintMarketsMinimumExpiryTime?: number;
+  stopsLimitsAllowed: boolean;
+  streamingPricesAvailable: boolean;
+  type:
+    | 'BUNGEE_COMMODITIES'
+    | 'BUNGEE_CURRENCIES'
+    | 'BUNGEE_INDICES'
+    | 'COMMODITIES'
+    | 'CURRENCIES'
+    | 'INDICES'
+    | 'OPT_COMMODITIES'
+    | 'OPT_CURRENCIES'
+    | 'OPT_INDICES'
+    | 'OPT_RATES'
+    | 'OPT_SHARES'
+    | 'RATES'
+    | 'SECTORS'
+    | 'SHARES'
+    | 'SPRINT_MARKET'
+    | 'TEST_MARKET'
+    | 'UNKNOWN';
+  unit: 'AMOUNT' | 'CONTRACTS' | 'SHARES';
+  valueOfOnePip: string;
+}
+
+export interface MinStepDistance {
+  unit: string;
+  value: number;
+}
+
+export interface MinDealSize {
+  unit: string;
+  value: number;
+}
+
+export interface MinControlledRiskStopDistance {
+  unit: string;
+  value: number;
+}
+
+export interface MinNormalStopOrLimitDistance {
+  unit: string;
+  value: number;
+}
+
+export interface MaxStopOrLimitDistance {
+  unit: string;
+  value: number;
+}
+
+export interface DealingRules {
+  marketOrderPreference: 'AVAILABLE_DEFAULT_OFF' | 'AVAILABLE_DEFAULT_ON' | 'NOT_AVAILABLE';
+  maxStopOrLimitDistance: MaxStopOrLimitDistance;
+  minControlledRiskStopDistance: MinControlledRiskStopDistance;
+  minDealSize: MinDealSize;
+  minNormalStopOrLimitDistance: MinNormalStopOrLimitDistance;
+  minStepDistance: MinStepDistance;
+  trailingStopsPreference: 'AVAILABLE' | 'NOT_AVAILABLE';
+}
+
+export interface Snapshot {
+  bid: number;
+  binaryOdds?: number;
+  controlledRiskExtraSpread: number;
+  decimalPlacesFactor: number;
+  delayTime: number;
+  high: number;
+  low: number;
+  marketStatus: 'CLOSED' | 'EDITS_ONLY' | 'OFFLINE' | 'ON_AUCTION' | 'ON_AUCTION_NO_EDITS' | 'SUSPENDED' | 'TRADEABLE';
+  netChange: number;
+  offer: number;
+  percentageChange: number;
+  scalingFactor: number;
+  updateTime: string;
+}
+
+export interface MarketDetail {
+  dealingRules: DealingRules;
+  instrument: Instrument;
+  snapshot: Snapshot;
+}
+
+export type MarketDetails = {marketDetails: MarketDetail[]};
+
 export interface MarketNode {
   id: string;
   name: string;
@@ -56,12 +203,29 @@ export class MarketAPI {
   /**
    * Returns all nodes (market categories) in the market navigation hierarchy.
    *
+   * @param nodeId - The identifier of the node to browse
    * @see https://labs.ig.com/rest-trading-api-reference/service-detail?id=550
    * @see https://labs.ig.com/rest-trading-api-reference/service-detail?id=544
    */
   async getMarketCategories(nodeId?: string): Promise<MarketNavigation> {
     const resource = nodeId ? `${MarketAPI.URL.MARKETNAVIGATION}/${nodeId}` : MarketAPI.URL.MARKETNAVIGATION;
     const response = await this.apiClient.get<MarketNavigation>(resource);
+    return response.data;
+  }
+
+  /**
+   * Returns the details of the given market(s).
+   *
+   * @param epic - The epic of the market to be retrieved
+   * @see https://labs.ig.com/rest-trading-api-reference/service-detail?id=528
+   */
+  async getMarketDetails(epic: string): Promise<MarketDetail>;
+  async getMarketDetails(epic: string[]): Promise<MarketDetails>;
+  async getMarketDetails(epic: string | string[]): Promise<MarketDetail | MarketDetails> {
+    const resource = Array.isArray(epic)
+      ? `${MarketAPI.URL.MARKETS}?epics=${encodeURIComponent(epic.join(','))}`
+      : `${MarketAPI.URL.MARKETS}/${epic}`;
+    const response = await this.apiClient.get(resource);
     return response.data;
   }
 }
